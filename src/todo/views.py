1b2e7304ef1deb1111c54d702c40bddd5ea05336
he3naika <dmitry.mihailovsky@gmail.com>
2017-10-17 09:24:54 +0000

    views.py was updated

--- src/todo/views.py
+++ src/todo/views.py
@@ -1,6 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
 
+from todo.models import TodoItem
+from todo.serializers import TodoItemSerializer
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.reverse import reverse
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
+
 # Create your views here.
+class TodoItemViewSet(viewsets.ModelViewSet):
+  queryset = TodoItem.objects.all()
+  serializer_class = TodoItemSerializer
+
+  # Save instance to get primary key and then update URL
+  def perform_create(self, serializer):
+    instance = serializer.save()
+    instance.url = reverse('todoitem-detail', args=[instance.pk], request=self.request)
+    instance.save()
+
+  # Delete all toso items
+  def delete(self, request):
+    TodoItem.objects.all().delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
