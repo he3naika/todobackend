ba0d09431313c1333958ac8d2ee397b982790039
he3naika <dmitry.mihailovsky@gmail.com>
2017-10-24 14:54:46 +0000

    'requirements' files were created

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,10 +3,11 @@
 . /appenv/bin/activate
 
 # Download requirements to build cache
-pip download -d /build -r requirements_test.txt --no-input
+#pip download -d /build -r requirements_test.txt --no-input
 
 # Install application test requirements
-pip install --no-index -f /build -r requirements_test.txt
+#pip install --no-index -f /build -r requirements_test.txt
+pip install -r requirements_test.txt
 
 # Run test.sh arguments
 exec $@
